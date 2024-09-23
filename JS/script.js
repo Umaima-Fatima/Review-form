8ec480c689614d5f25b2dbf1cc4d403b0e88fe6f
Umaima <98324267+Umaima-Fatima@users.noreply.github.com>
2024-09-23 23:29:06 +0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/*===================== Typing Animation =====================*/
+let typed = new Typed(".typing", {
+  strings:["", "Account Details.", "Personal Details.", "Review Section."],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
